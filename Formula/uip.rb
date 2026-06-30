d5e7c93b496f9e282c2c632b6fa30533a7688359
AndreiDinca24 <184099921+AndreiDinca24@users.noreply.github.com>
2026-06-30 19:57:20 +0000

    bump uip version to 1.196.3

--- Formula/uip.rb
+++ Formula/uip.rb
@@ -1,9 +1,9 @@
 class Uip < Formula
   desc "UiPath CLI for automation lifecycle management"
   homepage "https://www.npmjs.com/package/@uipath/cli"
-  version "1.196.0"
+  version "1.196.3"
   url "https://registry.npmjs.org/@uipath/cli/-/cli-#{version}.tgz"
-  sha256 "0d4d2dddbc4d57c1726af53c24279a09b9dcf71727b0d05f093d3226c41ca161"
+  sha256 "19af1a735af197a7a5d86a8841c287a04cffc7b4f0631d6b51bcdba7f3722e68"
 
   depends_on "node"
 
